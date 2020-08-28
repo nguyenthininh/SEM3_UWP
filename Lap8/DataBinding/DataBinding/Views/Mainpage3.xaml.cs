4ba88afa67258d81b7d80d1e1c99130ab68ad960
Nina Nguyen <Ninh2000md>
2020-08-28 11:45:29 +0700

    bai tap

---
+++ Lap8/DataBinding/DataBinding/Views/Mainpage3.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DataBinding.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DataBinding.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Mainpage3 : Page
+    {
+        public Mainpage3()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) => ViewModels = DataContext as MainPageViewModel;
+        }
+
+        public ViewModels.MainPageViewModel ViewModels { get; set; }
+    }
+}
